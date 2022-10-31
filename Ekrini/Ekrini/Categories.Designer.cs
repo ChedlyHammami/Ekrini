f0312bfc9556a809bc8fab481274762f42cff7dc
Chedly Hammami <56229742+ChedlyHammami@users.noreply.github.com>
2022-10-31 21:09:19 +0100

    Add files via upload

---
+++ Ekrini/Ekrini/Categories.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace Ekrini
+{
+    partial class Categories
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Categories));
+            this.pictureBox8 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox8
+            // 
+            this.pictureBox8.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox8.Image")));
+            this.pictureBox8.Location = new System.Drawing.Point(538, 12);
+            this.pictureBox8.Name = "pictureBox8";
+            this.pictureBox8.Size = new System.Drawing.Size(21, 21);
+            this.pictureBox8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox8.TabIndex = 9;
+            this.pictureBox8.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Pink;
+            this.panel1.Controls.Add(this.label8);
+            this.panel1.Location = new System.Drawing.Point(0, 39);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(571, 79);
+            this.panel1.TabIndex = 23;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.Color.Pink;
+            this.label8.Font = new System.Drawing.Font("Yu Gothic UI Semibold", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label8.ForeColor = System.Drawing.Color.Magenta;
+            this.label8.Location = new System.Drawing.Point(22, 11);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(115, 30);
+            this.label8.TabIndex = 10;
+            this.label8.Text = "Categories";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.BackColor = System.Drawing.Color.White;
+            this.label9.Font = new System.Drawing.Font("Bahnschrift", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label9.ForeColor = System.Drawing.Color.Magenta;
+            this.label9.Location = new System.Drawing.Point(168, 178);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(52, 19);
+            this.label9.TabIndex = 25;
+            this.label9.Text = "Name";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Yu Gothic UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox1.Location = new System.Drawing.Point(168, 200);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(208, 29);
+            this.textBox1.TabIndex = 24;
+            // 
+            // Categories
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(571, 376);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox8);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Categories";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Categories";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private PictureBox pictureBox8;
+        private Panel panel1;
+        private Label label8;
+        private Label label9;
+        private TextBox textBox1;
+    }
+}
---
+++ Ekrini/Ekrini/Tenants.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Runtime.InteropServices.ObjectiveC;
+
+namespace Ekrini
+{
+    public partial class Tenants : Form
+    {
+        public Tenants()
+        {
+            InitializeComponent();
+            showTenants();
+        }
+
+        SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\chedl\OneDrive\Documents\HouseRental.mdf;Integrated Security=True;Connect Timeout=30");
+        private void showTenants()
+        {
+            con.Open();
+            string Query = "select * from TenantTbl";
+            SqlDataAdapter sda = new SqlDataAdapter(Query, con);
+            SqlCommandBuilder Builer = new SqlCommandBuilder(sda);
+            var ds = new DataSet();
+            sda.Fill(ds);
+            TenantsDGV.DataSource = ds.Tables[0];
+            con.Close();
+        }
+     
+        private void ResetData()
+        {
+            phoneTb.Text = "";
+            GenCb.SelectedIndex = -1;
+            TnameTb.Text = "";
+        }
+        private void Savebtn_Click(object sender, EventArgs e)
+        {
+            if(TnameTb.Text == "" || GenCb.SelectedIndex == -1 || phoneTb.Text == "")
+            {
+                MessageBox.Show("Missing Information !");
+            }
+            else
+            {
+                try
+                {
+                    con.Open();
+                    SqlCommand cmd = new SqlCommand("insert into TenantTbl(TenName, TenPhone, TenGen) values(@TN,@TP,@TG)",con);
+                    cmd.Parameters.AddWithValue("@TN", TnameTb.Text);
+                    cmd.Parameters.AddWithValue("@TP", phoneTb.Text);
+                    cmd.Parameters.AddWithValue("@TG", GenCb.SelectedItem.ToString());
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("Tenant Added !");
+                    con.Close();
+                    ResetData();
+                    showTenants();
+                }
+                catch (Exception Ex)
+                {
+                    MessageBox.Show(Ex.Message);
+                }
+
+            }
+        }
+        int key = 0;
+        private void TenantsDGV_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            TnameTb.Text = TenantsDGV.SelectedRows[0].Cells[1].Value.ToString();
+            phoneTb.Text = TenantsDGV.SelectedRows[0].Cells[2].Value.ToString();
+            GenCb.Text = TenantsDGV.SelectedRows[0].Cells[3].Value.ToString();
+            if (TnameTb.Text == "")
+            {
+                key = 0;
+            }
+            else
+            {
+                key = Convert.ToInt32(TenantsDGV.SelectedRows[0].Cells[0].Value.ToString());
+            }
+
+        }
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void guna2GradientButton2_Click(object sender, EventArgs e)
+        {
+            if(key == 0)
+            {
+                MessageBox.Show("Select A Tenant");
+            }
+            else
+            {
+                try
+                {
+                    con.Open();
+                    SqlCommand cmd = new SqlCommand("delete from TenantTbl where TenId=@Tkey", con);
+                    cmd.Parameters.AddWithValue("@Tkey", key);
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("Tenant Deleted !");
+                    con.Close();
+                    ResetData();
+                    showTenants();
+                }
+                catch(Exception Ex)
+                {
+                    MessageBox.Show(Ex.Message);
+                }
+            }
+        }
+
+        private void guna2GradientButton1_Click(object sender, EventArgs e)
+        {
+            if (TnameTb.Text == "" || GenCb.SelectedIndex == -1 || phoneTb.Text == "")
+            {
+                MessageBox.Show("Missing Information !");
+            }
+            else
+            {
+                try
+                {
+                    con.Open();
+                    SqlCommand cmd = new SqlCommand("update TenantTbl set TenName=@TN, TenPhone=@TP, TenGen=@TG where TenId=@Tkey", con);
+                    cmd.Parameters.AddWithValue("@TN", TnameTb.Text);
+                    cmd.Parameters.AddWithValue("@TP", phoneTb.Text);
+                    cmd.Parameters.AddWithValue("@TG", GenCb.SelectedItem.ToString());
+                    cmd.Parameters.AddWithValue("@Tkey", key);
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("Tenant Updated !");
+                    con.Close();
+                    ResetData();
+                    showTenants();
+                }
+                catch (Exception Ex)
+                {
+                    MessageBox.Show(Ex.Message);
+                }
+
+            }
+        }
+    }
+}
